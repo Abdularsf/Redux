42b75dcd8b097cd1987a27007559fe48a9b42c37
Abdul Rahman <abdularsf147@gmail.com>
2025-12-22 23:25:17 +0530

    simple boiler plate

---
+++ src/store.jsx
@@ -0,0 +1,22 @@
+const ADD_TASK = "task/add"
+const DELETE_TASK = "task/delete"
+
+const initialState ={
+    task:[],
+}
+
+const taskReducer = (state = initialState,action) =>{
+    switch (action.type) {
+        case ADD_TASK:
+            return{
+                ...state,task:[...state.task,action.payload],
+            }
+        case DELETE_TASK:
+            return{
+                ...state,task:[...state.task,action.payload],
+            }
+    
+        default:
+            break;
+    }
+}
